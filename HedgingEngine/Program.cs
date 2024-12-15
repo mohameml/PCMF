5daa40152e1827b542c991c199056fbf12aeb5e4
mohameml <mhmdahmdnbyl18@gmail.com>
2024-12-15 14:21:47 +0100

    update

---
+++ HedgingEngine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcPricing.Protos;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // URL de votre serveur gRPC
+        var channel = GrpcChannel.ForAddress("http://localhost:50051");
+
+        // Créer un client pour le service Greeter
+        var client = new GrpcPricer.GrpcPricerClient(channel);
+
+        // Effectuer un appel au service
+        var reply = await client.HeartbeatAsync(new Empty());
+        Console.WriteLine("Message reçu : " + reply.DomesticInterestRate);
+        Console.WriteLine("Message reçu : " + reply.RelativeFiniteDifferenceStep);
+        Console.WriteLine("Message reçu : " + reply.SampleNb);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# PCMF
-projet couverture multi-flux 
+# PCMF : projet couverture multi-flux 
+
+- **Etapes principales du projet:**
+
+    - Faire valider l'architecture et le principe mathématique de couverture
+    - Réussir à faire communiquer le client et le serveur grpc (squelettes)
+    - Implémentation d'un outil qui couvre un call vanille
+    - Implémentation d'un outil qui couvre les deux produits décrits au dessus.
